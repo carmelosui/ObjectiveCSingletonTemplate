ab847bac196a1ffa89fa17e4d6ed7a35417e3646
Carmelo Sui <Carmelosui@gmail.com>
2014-05-16 18:37:23 +0800

    add non-ARC singltons

---
+++ CMLObjc/non-ARC singleton without thread safety.xctemplate/NSObject/___FILEBASENAME___.m
@@ -0,0 +1,57 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
++(___FILEBASENAMEASIDENTIFIER___*)sharedInstance
+{
+    static ___FILEBASENAMEASIDENTIFIER___ *instance = nil;
+    if (!instance) {
+        instance = [[___FILEBASENAMEASIDENTIFIER___ alloc] init];
+    }
+    return instance;
+}
+
+#pragma mark Singleton Overrides
+
++ (id)allocWithZone:(NSZone *)zone 
+{
+  return [[self sharedInstance] retain];
+}
+
+- (id)copyWithZone:(NSZone *)zone 
+{
+  return self;
+}
+
+- (id)retain 
+{
+  return self;
+}
+
+- (unsigned)retainCount 
+{
+  return UINT_MAX;
+}
+
+- (oneway void)release 
+{
+  // never release
+}
+
+- (id)autorelease 
+{
+  return self;
+}
+
+- (void)dealloc 
+{
+  [super dealloc];
+}
